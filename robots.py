b31e1a61805f33e4e207df5da19b3be6ec817cea
Danel Lafuente <dlafuente003@gmail.com>
2018-03-12 16:44:03 +0100

    Add files via upload

---
+++ robots.py
@@ -0,0 +1,34 @@
+#import drowsy
+
+
+moods=["happy","sad","surprised","neutral","angry"]
+
+
+class Robot:
+	def __init__(self):
+		pass
+		self.drowsy=drowsy.Drowc()
+
+	def getMood():
+		result="sad"
+
+		return result
+	def initRecognition():
+		t = Thread(target=self.loop)
+        t.deamon = True
+        t.start()
+	def loop():
+		vs = VideoStream(0).start()
+		while(d.detect(vs.read())!=-1):
+    		pass
+		cv2.destroyAllWindows()
+		vs.stop() 
+
+
+
+
+
+
+
+
+
